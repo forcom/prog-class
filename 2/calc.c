032d7c00ab4e42a030dc2898a302f47300d6193d
Yoon SeungYong <un6549@gmail.com>
2012-06-15 13:34:46 +0900

    Practice 2: Completed with print_command

--- 2/calc.c
+++ 2/calc.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+short exit = 0;
+
+void print_command ( ) {
+	printf ( "+-*/()= MR[M] MC[R] CLR[C] CE[D]\n>> " ) ;
+}
+
+void input ( ) {
+}
+
+void process ( ) {
+}
+
+void output ( ) {
+}
+
 int main ( ) {
+	while ( ! exit ) {
+		print_command ( ) ;
+		input ( ) ;
+		process ( ) ;
+		output ( ) ;
+	}
 	return 0 ;
 }
